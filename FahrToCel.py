ea1676cbf2b086620c914cf3a62d6fd05940a23d
alexisfaison <avfaison@loyola.edu>
2021-09-23 14:34:57 -0400

    Initial commit 1

---
+++ FahrToCel.py
@@ -0,0 +1,7 @@
+#FahrToCel.py
+#This program will convert a temperature
+#in Fahrenheit to Celcius
+Fahr = input("Enter the temperature in Fahrenheit: ")
+Fahr = float(Fahr)
+Cel = (Fahr - 32)/1.8
+print("Temperature in Celcius: ", Cel)
